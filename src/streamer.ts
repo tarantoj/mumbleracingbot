f6c9223ecb82cebdc4e7202834da0067dd9febf8
James Taranto <taranto.james@gmail.com>
2020-09-27 02:50:05 +1000

    ffmpeg logging

--- src/streamer.ts
+++ src/streamer.ts
@@ -53,7 +53,7 @@ export default class Streamer {
 
     this.childProcess = spawn(pathToFfmpeg, args, { shell: true });
 
-    this.childProcess?.stderr?.on('data', (data) => logger.warn(data));
+    this.childProcess?.stderr?.on('data', (data) => logger.warn(`${data}`));
   }
 
   // eslint-disable-next-line no-unused-vars
