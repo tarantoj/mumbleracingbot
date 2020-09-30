a65ccc303ca04ebc6c4e8cac2245d03483168931
James Taranto <taranto.james@gmail.com>
2020-09-30 20:56:13 +1000

    format logs

--- src/logger.ts
+++ src/logger.ts
@@ -6,4 +6,9 @@ export default winston.createLogger({
       level: process.env.LOG_LEVEL || 'error',
     }),
   ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.prettyPrint(),
+  ),
 });
