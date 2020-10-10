8951d1091b544b4d32bd9d510fce7451c85b89b9
James Taranto <taranto.james@gmail.com>
2020-10-10 22:05:18 +1100

    run through prettier

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,118 @@
-import * as _ from 'lodash';
-import * as tmi from 'tmi.js';
-import * as dotenv from 'dotenv';
-import Streamer from './streamer';
-import logger from './logger';
+import * as _ from "lodash";
+import * as tmi from "tmi.js";
+import * as dotenv from "dotenv";
+import Streamer from "./streamer";
+import logger from "./logger";
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: ".env" });
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'prod';
+process.env.NODE_ENV = process.env.NODE_ENV || "prod";
 
 const twitchOpts: tmi.Options = {
   identity: {
     username: process.env.BOT_USERNAME,
     password: process.env.OAUTH_TOKEN,
   },
-  channels: process.env.CHANNEL_NAMES?.split(','),
+  channels: process.env.CHANNEL_NAMES?.split(","),
 };
 
 const client = tmi.Client(twitchOpts);
 
-client.connect()
+client
+  .connect()
   .then(() => {
-    client.say(process.env.CHANNEL_NAMES?.split(',')[0] || 'mumbleracing', 'bot connected!');
-    logger.info('connected!');
+    client.say(
+      process.env.CHANNEL_NAMES?.split(",")[0] ?? "mumbleracing",
+      "bot connected!"
+    );
+    logger.info("connected!");
   })
   .catch((reason) => logger.error(`Connect failed with ${reason}`));
 
 const reminderCallback = (channel: string) => {
-  logger.info('Reminder triggered');
-  client.say(channel, `Still watching? Send \'!stillhere\' to keep the streaming going for another ${process.env.STOP_TIME ?? 60} mins.`)
+  logger.info("Reminder triggered");
+  client
+    .say(
+      channel,
+      `Still watching? Send \'!stillhere\' to keep the streaming going for another ${
+        process.env.STOP_TIME ?? 60
+      } mins.`
+    )
     .then()
     .catch((reason) => logger.error(`Reminder say failed with ${reason}`));
 };
 
-const messageListener = (channel: string,
+const messageListener = (
+  channel: string,
   userstate: tmi.ChatUserstate,
   message: String,
-  self: boolean) => {
-  logger.info(`channel: ${channel}, username: ${userstate.username}, message: ${message}, self: ${self}`);
+  self: boolean
+) => {
+  logger.info(
+    `channel: ${channel}, username: ${userstate.username}, message: ${message}, self: ${self}`
+  );
 
   if (self) return;
-  if (!_.includes(process.env.AUTHORISED_USERS?.split(','), userstate.username?.toLowerCase())) return;
+  if (
+    !_.includes(
+      process.env.AUTHORISED_USERS?.split(","),
+      userstate.username?.toLowerCase()
+    )
+  )
+    return;
 
-  if (message.startsWith('!switch')) {
-    const chanNum = Number(message.split(' ')[1]);
-    if (!_.includes(process.env.AVAILABLE_CHANNELS?.split(',').map((c) => Number(c)), chanNum)) return;
-    if (process.env.NODE_ENV === 'prod') {
+  if (message.startsWith("!switch")) {
+    const chanNum = Number(message.split(" ")[1]);
+    if (
+      !_.includes(
+        process.env.AVAILABLE_CHANNELS?.split(",").map((c) => Number(c)),
+        chanNum
+      )
+    )
+      return;
+    if (process.env.NODE_ENV === "prod") {
       Streamer.getInstance().change(chanNum, () => reminderCallback(channel));
     }
     logger.info(`Got request to change to ${chanNum}`);
     client.say(channel, `Changing channel to ${chanNum}`);
   }
 
-  if (message.startsWith('!stillhere')) {
-    if (process.env.NODE_ENV === 'prod') Streamer.getInstance().stillHere(() => reminderCallback(channel));
-    logger.info('Got request to keep streaming');
-    client.say(channel, `Got it! Will keep streaming for another ${process.env.STOP_TIME ?? 60} mins.`);
+  if (message.startsWith("!stillhere")) {
+    if (process.env.NODE_ENV === "prod")
+      Streamer.getInstance().stillHere(() => reminderCallback(channel));
+    logger.info("Got request to keep streaming");
+    client.say(
+      channel,
+      `Got it! Will keep streaming for another ${
+        process.env.STOP_TIME ?? 60
+      } mins.`
+    );
   }
 
-  if (message.startsWith('!stop')) {
-    if (process.env.NODE_ENV === 'prod') Streamer.getInstance().stop().catch(() => process.exit());
-    logger.info('Got request to stop streaming');
-    client.say(channel, 'Got it! Will stop streaming.');
+  if (message.startsWith("!stop")) {
+    if (process.env.NODE_ENV === "prod")
+      Streamer.getInstance()
+        .stop()
+        .catch(() => process.exit());
+    logger.info("Got request to stop streaming");
+    client.say(channel, "Got it! Will stop streaming.");
   }
 
-  if (message.startsWith('!chans')) {
-    logger.info('!chans');
-    client.say(channel, process.env.AVAILABLE_CHANNELS?.split(',').toString() || 'None! :(');
+  if (message.startsWith("!chans")) {
+    logger.info("!chans");
+    client.say(
+      channel,
+      process.env.AVAILABLE_CHANNELS?.split(",").toString() || "None! :("
+    );
   }
 
-  if (message.startsWith('!help')) {
-    logger.info('!help');
-    client.say(channel, '!help: Print this message');
-    client.say(channel, '!switch <channel>: Switch to channel specified');
-    client.say(channel, '!stop: Stop streaming');
-    client.say(channel, '!chans: Get a list of available channels');
+  if (message.startsWith("!help")) {
+    logger.info("!help");
+    client.say(channel, "!help: Print this message");
+    client.say(channel, "!switch <channel>: Switch to channel specified");
+    client.say(channel, "!stop: Stop streaming");
+    client.say(channel, "!chans: Get a list of available channels");
   }
 };
 
-client.on('message', messageListener);
+client.on("message", messageListener);
--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,14 @@
-import winston from 'winston';
+import winston from "winston";
 
 export default winston.createLogger({
   transports: [
     new winston.transports.Console({
-      level: process.env.LOG_LEVEL || 'error',
+      level: process.env.LOG_LEVEL ?? "error",
     }),
   ],
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.prettyPrint(),
+    winston.format.prettyPrint()
   ),
 });
